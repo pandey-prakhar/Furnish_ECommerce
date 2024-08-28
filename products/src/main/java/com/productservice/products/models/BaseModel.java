e0f1fc664d6a7e322b9c3c15754142f775c88594
pandey-prakhar <prakharpandey1198@gmail.com>
2024-08-28 09:10:49 +0530

    Created a Self product service to replace proxy server.

--- products/src/main/java/com/productservice/products/models/BaseModel.java
+++ products/src/main/java/com/productservice/products/models/BaseModel.java
@@ -5,13 +5,10 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
-
-@Setter
 @Getter
+@Setter
 @Entity
-public class Category extends BaseModel {
-    private String description;
-    private Date createdAt;
-    private Date updatedAt;
+public class BaseModel {
+    @Id
+    private Long id;
 }
--- products/src/main/java/com/productservice/products/models/Product.java
+++ products/src/main/java/com/productservice/products/models/Product.java
@@ -1,15 +1,20 @@
 package com.productservice.products.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jdk.jfr.Enabled;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product{
-    private Long id;
+@Entity
+public class Product extends BaseModel{
     private String title;
     private String description;
     private String image;
     private double price;
+    @ManyToOne
     private Category category;
 }
