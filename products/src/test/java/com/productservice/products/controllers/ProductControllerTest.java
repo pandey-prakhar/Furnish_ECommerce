ac79f7082707c236423318b9af84691a5556f65e
pandey-prakhar <prakharpandey1198@gmail.com>
2025-01-07 23:11:44 +0530

    Added test for replaceProduct API.

--- products/src/test/java/com/productservice/products/controllers/ProductControllerTest.java
+++ products/src/test/java/com/productservice/products/controllers/ProductControllerTest.java
@@ -23,6 +23,34 @@ class ProductControllerTest {
     @MockBean(name = "selfProductService")
     ProductService productService;
 
+    //Testing getAllProducts endpoint.
+    @Test
+    void validGetAllProducts(){
+        //Arrange
+        ArrayList<Product> productsFromService = new ArrayList<>();
+        Product product1 = new Product();
+        product1.setId(1L);
+        product1.setTitle("Hinge from Cryatal");
+        product1.setDescription("Hinges");
+
+        Product product2 = new Product();
+        product2.setId(2L);
+        product2.setTitle("Hinges from Fabista");
+        product2.setDescription("Hinges");
+
+        productsFromService.add(product1);
+        productsFromService.add(product2);
+        when(productService.getAllProducts()).thenReturn(productsFromService);
+
+        //Act
+        ProductController productController = new ProductController(productService);
+        List<Product> products = productController.getAllProducts().getBody();
+
+        //Assert
+        assertIterableEquals(productsFromService, products);
+    }
+
+    //Testing getProductById endpoint.
     @Test
     void validGetProductById() throws ProductNotFoundExeption {
         //Arrange
@@ -48,30 +76,39 @@ class ProductControllerTest {
         assertThrows(ProductNotFoundExeption.class, () -> productController.getProductById(productId).getBody());
     }
 
+    //Testing replaceProducts endpoint.
     @Test
-    void validGetAllProducts(){
+    void testValidReplaceProduct() throws ProductNotFoundExeption {
         //Arrange
-        ArrayList<Product> productsFromService = new ArrayList<>();
-        Product product1 = new Product();
-        product1.setId(1L);
-        product1.setTitle("Hinge from Cryatal");
-        product1.setDescription("Hinges");
+        Product originalProduct = new Product();
+        originalProduct.setId(1L);
+        originalProduct.setTitle("original Title");
+        originalProduct.setPrice(130.0);
 
-        Product product2 = new Product();
-        product2.setId(2L);
-        product2.setTitle("Hinges from Fabista");
-        product2.setDescription("Hinges");
+        Product passedProduct = new Product();
+        passedProduct.setTitle("passed Title");
+        passedProduct.setPrice(135.0);
 
-        productsFromService.add(product1);
-        productsFromService.add(product2);
-        when(productService.getAllProducts()).thenReturn(productsFromService);
+        Product updatedProduct = new Product();
+        updatedProduct.setId(1L);
+        updatedProduct.setTitle(passedProduct.getTitle());
+        updatedProduct.setPrice(passedProduct.getPrice());
+
+        when(productService.replaceProduct(1L,passedProduct)).thenReturn(updatedProduct);
+        ProductController productController = new ProductController(productService);
 
         //Act
-        ProductController productController = new ProductController(productService);
-        List<Product> products = productController.getAllProducts().getBody();
+        ResponseEntity<Product> returnedProduct = productController.replaceProduct(1L,passedProduct);
 
         //Assert
-        assertIterableEquals(productsFromService, products);
-
+        assertEquals(200, returnedProduct.getStatusCodeValue());
+        assertEquals(updatedProduct, returnedProduct.getBody());
     }
+
+
+
+
+
+
+
 }
